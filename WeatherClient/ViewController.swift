9f399cb5bd081d0e96c9da3b887941a6664b73d1
George Prokopenko <green.mobile@mail.ru>
2020-02-15 18:56:34 +0300

    Initial Commit

---
+++ WeatherClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherClient
+//
+//  Created by George Prokopenko on 15/02/2020.
+//  Copyright © 2020 George Prokopenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
